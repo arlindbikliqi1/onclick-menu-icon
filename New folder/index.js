5ad66bf8d8b47725a7806b55d03bedc3518505ba
arlindbikliqi1 <125808167+arlindbikliqi1@users.noreply.github.com>
2023-04-15 16:29:10 +0200

    Add files via upload

---
+++ New folder/index.js
@@ -0,0 +1,12 @@
+let a;
+function hape(){
+  if(a==1)  
+    {
+        document.getElementById("list").style.display="inline";
+        return a=0;
+    }
+    else{
+        document.getElementById("list").style.display="none";
+        return a=1;
+    }
+}
